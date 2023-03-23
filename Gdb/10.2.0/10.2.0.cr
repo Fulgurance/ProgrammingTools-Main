2ed574501757ede19377a3b33875b3c4a84e1202
fulgurance <zohran.londais@gmail.com>
2023-03-23 23:33:30 +0000

    Added Gdb installer

---
+++ Gdb/10.2.0/10.2.0.cr
@@ -0,0 +1,29 @@
+class Target < ISM::Software
+    
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        configureSource([   "--prefix=/usr",
+                            "--with-system-readline",
+                            "--with-python=/usr/bin/python3"],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","-C","gdb","install"],buildDirectoryPath)
+    end
+
+end
