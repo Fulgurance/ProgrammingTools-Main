586f8289187fe76ce24efbe2087c317c9fe2c8e5
fulgurance <zohran.londais@gmail.com>
2023-03-29 11:37:20 +0100

    Patched wrong path for chroot installation

--- DejaGnu/1.6.3/1.6.3.cr
+++ DejaGnu/1.6.3/1.6.3.cr
@@ -7,24 +7,26 @@ class Target < ISM::Software
 
     def configure
         super
+
         configureSource([   "--prefix=/usr"],
                             buildDirectoryPath)
     end
 
     def prepareInstallation
         super
+
         runMakeinfoCommand(["--html",
                             "--no-split",
                             "-o",
                             "doc/dejagnu.html",
                             "../doc/dejagnu.texi"],
-                            buildDirectoryPath(false))
+                            buildDirectoryPath)
         runMakeinfoCommand(["--plaintext",
                             "-o",
                             "doc/dejagnu.txt",
                             "../doc/dejagnu.texi"],
-                            buildDirectoryPath(false))
-        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+                            buildDirectoryPath)
+        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
         makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/dejagnu-1.6.3")
         moveFile("#{buildDirectoryPath(false)}/doc/dejagnu.html","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/share/doc/dejagnu-1.6.3/dejagnu.html")
         moveFile("#{buildDirectoryPath(false)}/doc/dejagnu.txt","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/share/doc/dejagnu-1.6.3/dejagnu.txt")
