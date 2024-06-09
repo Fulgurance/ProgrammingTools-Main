9d2bc4c65d89cdedb2e1557357276e9553a44aed
Fulgurance <zohran.londais@gmail.com>
2024-06-09 23:14:39 +0100

    Removed useless software

--- KBuild/0.1.5-p.2/0.1.5-p.2.cr
+++
@@ -1,9 +0,0 @@
-class Target < ISM::Software
-    
-    def prepareInstallation
-        super
-
-        moveFile("#{mainWorkDirectoryPath(false)}/bin","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/bin")
-    end
-
-end
