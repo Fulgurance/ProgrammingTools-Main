1b2d8b45234589fa64052b45b25a41d00b97f46f
Fulgurance <zohran.londais@gmail.com>
2024-06-11 22:52:04 +0100

    Patched wrong accessor name

--- Gcc/13.2.0/13.2.0.cr
+++ Gcc/13.2.0/13.2.0.cr
@@ -176,8 +176,8 @@ class Target < ISM::Software
 
             runChownCommand(["-R","root:root","#{Ism.settings.rootPath}usr/lib/gcc/#{Ism.settings.systemTarget}/linux-gnu/13.2.0/include"])
             runChownCommand(["-R","root:root","#{Ism.settings.rootPath}usr/lib/gcc/#{Ism.settings.systemTarget}/linux-gnu/13.2.0/include-fixed"])
-            runChownCommand(["-R","root:root","#{Ism.settings.rootPath}usr/lib/gcc/#{Ism.settings.architecture}-pc-linux-gnu/13.2.0/include"])
-            runChownCommand(["-R","root:root","#{Ism.settings.rootPath}usr/lib/gcc/#{Ism.settings.architecture}-pc-linux-gnu/13.2.0/include-fixed"])
+            runChownCommand(["-R","root:root","#{Ism.settings.rootPath}usr/lib/gcc/#{Ism.settings.systemArchitecture}-pc-linux-gnu/13.2.0/include"])
+            runChownCommand(["-R","root:root","#{Ism.settings.rootPath}usr/lib/gcc/#{Ism.settings.systemArchitecture}-pc-linux-gnu/13.2.0/include-fixed"])
         end
     end
 
