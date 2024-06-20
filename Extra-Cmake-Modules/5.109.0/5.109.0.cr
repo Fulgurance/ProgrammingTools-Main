72eac1c9972ec835f885255113eb0d11fd7e44a6
Fulgurance <zohran.londais@gmail.com>
2024-06-20 22:12:43 +0100

    Patched installer

--- Extra-Cmake-Modules/5.109.0/5.109.0.cr
+++ Extra-Cmake-Modules/5.109.0/5.109.0.cr
@@ -3,19 +3,6 @@ class Target < ISM::Software
     def prepare
         @buildDirectory = true
         super
-
-        fileReplaceTextAtLineNumber(path:       "#{mainWorkDirectoryPath}/kde-modules/KDEInstallDirsCommon.cmake",
-                                    text:       "set(_LIBDIR_DEFAULT \"lib64\")",
-                                    newText:    "set(_LIBDIR_DEFAULT \"lib\")",
-                                    lineNumber: 44)
-
-        fileReplaceText(path:       "#{mainWorkDirectoryPath}/ECMConfig.cmake.in",
-                        text:       "@PACKAGE_INIT@",
-                        newText:    "set(SAVE_PACKAGE_PREFIX_DIR \"${PACKAGE_PREFIX_DIR}\")\n@PACKAGE_INIT@")
-
-        fileReplaceText(path:       "#{mainWorkDirectoryPath}/ECMConfig.cmake.in",
-                        text:       "include(\"${ECM_MODULE_DIR}/ECMUseFindModules.cmake\")",
-                        newText:    "include(\"${ECM_MODULE_DIR}/ECMUseFindModules.cmake\")\nset(PACKAGE_PREFIX_DIR \"${SAVE_PACKAGE_PREFIX_DIR}\")")
     end
     
     def configure
