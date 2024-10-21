a516a741a8e43d9563b0cd736e1bbd5c293b3ced
Fulgurance <zohran.londais@gmail.com>
2024-10-21 13:51:56 +0100

    Updated installation process

--- Meson/1.2.1/1.2.1.cr
+++ Meson/1.2.1/1.2.1.cr
@@ -1,19 +1,2 @@
-class Target < ISM::Software
-    
-    def build
-        super
-
-        runPythonCommand(   arguments:  "setup.py build",
-                            path:       buildDirectoryPath)
-    end
-    
-    def prepareInstallation
-        super
-
-        runPythonCommand(   arguments:  "setup.py install --root=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}",
-                            path:       buildDirectoryPath)
-
-        makeDirectory("#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/usr/share/bash-completion/completions/meson")
-    end
-
+class PythonPackageSoftware < ISM::SemiVirtualSoftware
 end
