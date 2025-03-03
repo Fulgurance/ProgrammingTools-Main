8da3b574412c1241cba8d72580411ffc436c988f
Fulgurance <zohran.londais@gmail.com>
2025-03-03 15:21:32 +0000

    Removed documentation

--- DejaGnu/1.6.3/1.6.3.cr
+++ DejaGnu/1.6.3/1.6.3.cr
@@ -15,36 +15,8 @@ class Target < ISM::Software
     def prepareInstallation
         super
 
-        runMakeinfoCommand(arguments:   "--html             \
-                                        --no-split          \
-                                        -o                  \
-                                        doc/dejagnu.html    \
-                                        ../doc/dejagnu.texi",
-                            path:       buildDirectoryPath)
-
-        runMakeinfoCommand( arguments:  "--plaintext    \
-                                        -o              \
-                                        doc/dejagnu.txt \
-                                        ../doc/dejagnu.texi",
-                            path:       buildDirectoryPath)
-
         makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
                     path:       buildDirectoryPath)
-
-        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/share/doc/#{versionName}")
-
-        moveFile(   "#{buildDirectoryPath}/doc/dejagnu.html",
-                    "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/share/doc/#{versionName}/dejagnu.html")
-        moveFile(   "#{buildDirectoryPath}/doc/dejagnu.txt",
-                    "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/share/doc/#{versionName}/dejagnu.txt")
-    end
-
-    def install
-        super
-
-        runChmodCommand("0755 /usr/share/doc/#{versionName}")
-        runChmodCommand("0644 /usr/share/doc/#{versionName}/dejagnu.html")
-        runChmodCommand("0644 /usr/share/doc/#{versionName}/dejagnu.txt")
     end
 
 end
