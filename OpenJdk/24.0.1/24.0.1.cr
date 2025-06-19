d33041ef5b82e47b01334b83887f256dc1a0ea9c
Fulgurance <zohran.londais@gmail.com>
2025-06-19 11:15:13 +0100

    Finished to implement openjdk installer

--- OpenJdk/24.0.1/24.0.1.cr
+++ OpenJdk/24.0.1/24.0.1.cr
@@ -16,6 +16,7 @@ class Target < ISM::Software
                                     --with-libpng=system                            \
                                     --with-lcms=system                              \
                                     --with-zlib=system                              \
+                                    --enable-unlimited-crypto                       \
                                     --with-harfbuzz=system",
                         path:       buildDirectoryPath)
     end
@@ -23,14 +24,54 @@ class Target < ISM::Software
     def build
         super
 
-        makeSource(path: buildDirectoryPath)
+        makeSource( arguments: "images",
+                    path: buildDirectoryPath)
     end
 
     def prepareInstallation
         super
 
-        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
-                    path:       buildDirectoryPath)
+        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/opt")
+
+        moveDirectory(  "#{buildDirectoryPath}/build/*/images/jdk",
+                        "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/opt/jdk-#{version}")
+
+        ###########################
+        if File.exists?("#{Ism.settings.rootPath}etc/profile.d/qt.sh")
+            copyFile(   "/etc/profile.d/qt.sh",
+                        "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/qt.sh")
+        else
+            generateEmptyFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/qt.sh")
+        end
+
+        jdkData = <<-CODE
+        JAVA_HOME=/opt/jdk
+        AUTO_CLASSPATH_DIR=/usr/share/java
+
+        pathappend $JAVA_HOME/bin
+
+        for dir in `find ${AUTO_CLASSPATH_DIR} -type d 2>/dev/null`; do
+            pathappend $dir CLASSPATH
+        done
+
+        for jar in `find ${AUTO_CLASSPATH_DIR} -name "*.jar" 2>/dev/null`; do
+            pathappend $jar CLASSPATH
+        done
+
+        _JAVA_OPTIONS="-XX:-UsePerfData"
+
+        export JAVA_HOME
+        export _JAVA_OPTIONS
+        unset AUTO_CLASSPATH_DIR dir jar _JAVA_OPTIONS
+        CODE
+        fileUpdateContent("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/jdk.sh",jdkData)
+        ###########################
+
+        if isGreatestVersion
+            makeLink(   target: "jdk-#{version}",
+                        path:   "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/opt/jdk",
+                        type:   :symbolicLink)
+        end
     end
 
 end
