88758aadcad75269bd0aed3f010a175f9989eccb
Fulgurance <zohran.londais@gmail.com>
2025-07-20 15:02:51 +0100

    Added musl-locales package

---
+++ Musl-Locales/0.1.0/0.1.0.cr
@@ -0,0 +1,30 @@
+class Target < ISM::Software
+    
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr    \
+                                    -DCMAKE_BUILD_TYPE=Release      \
+                                    ..",
+                        path:       buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
+                    path:       buildDirectoryPath)
+    end
+
+end
